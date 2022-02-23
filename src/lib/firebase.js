f132cb71d3a32ea984de0cba8d1e8124f3e95606
karicheslock <75226288+karicheslock@users.noreply.github.com>
2022-02-23 09:27:58 -0800

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 
 // firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyB3VtsKUvW6PANw-u8KpRQ8ZchFioVCDhA",
-  authDomain: "instagram-kari.firebaseapp.com",
-  projectId: "instagram-kari",
-  storageBucket: "instagram-kari.appspot.com",
-  messagingSenderId: "147796865772",
-  appId: "1:147796865772:web:372d3586cd7f68f8ebabc9"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
 
 // Initialize Firebase
